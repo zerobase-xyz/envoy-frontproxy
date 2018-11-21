489fb5b90477ae293c2622aa98d6491e70d3d385
sumir <offroadrider.mx.0723@gmail.com>
2018-11-21 22:59:16 +0900

    commit

--- app-b/app.py
+++ app-b/app.py
@@ -12,41 +12,44 @@ SERVICE_NAME = os.environ['SERVICE_NAME']
 REDIS_HOST = os.environ['REDIS_HOST']
 conn = redis.Redis(host=REDIS_HOST, port=6379)
 
-@app.route('/app/<service_number>')
-def hello(service_number):
+# hostnameと環境変数に設定したSERVICE_NAMEを返す(aとかb)
+@app.route('/app/<app_number>')
+def hello(app_number):
     return ('Hello from behind Envoy (service {})! hostname: {} resolved'
             'hostname: {}\n'.format(SERVICE_NAME,
                                     socket.gethostname(),
                                     socket.gethostbyname(socket.gethostname())))
 
-@app.route('/app/<service_number>/view', methods=['GET'])
-def redis_view(service_number):
+# redisのincrementされた数を表示する
+@app.route('/app/<app_number>/view', methods=['GET'])
+def redis_view(app_number):
     times = conn.get('carats')
     return ('Hello from behind Envoy (service {})! hostname: {} resolved'
             'hostname: {}\napp {}times\n'.format(SERVICE_NAME,
-                                    socket.gethostname(),
-                                    socket.gethostbyname(socket.gethostname()),
-                                    int(times)))
+                                                 socket.gethostname(),
+                                                 socket.gethostbyname(socket.gethostname()),
+                                                 int(times)))
 
-@app.route('/app/<service_number>/incr', methods=['GET'])
-def redis_incr(service_number):
+# redisをincrementする
+@app.route('/app/<app_number>/incr', methods=['GET'])
+def redis_incr(app_number):
     times = conn.incr('carats')
     return ('Hello from behind Envoy (service {})! hostname: {} resolved'
             'hostname: {}\napp {}times\n'.format(SERVICE_NAME,
-                                    socket.gethostname(),
-                                    socket.gethostbyname(socket.gethostname()),
-                                    int(times)))
+                                                 socket.gethostname(),
+                                                 socket.gethostbyname(socket.gethostname()),
+                                                 int(times)))
 
-
-@app.route('/app/<service_number>/init', methods=['GET'])
-def redis_init(service_number):
+# redisを初期化する
+@app.route('/app/<app_number>/init', methods=['GET'])
+def redis_init(app_number):
     conn.set('carats', 0)
     times = conn.get('carats')
     return ('Hello from behind Envoy (service {})! hostname: {} resolved'
             'hostname: {}\napp {}times\n'.format(SERVICE_NAME,
-                                    socket.gethostname(),
-                                    socket.gethostbyname(socket.gethostname()),
-                                    int(times)))
+                                                 socket.gethostname(),
+                                                 socket.gethostbyname(socket.gethostname()),
+                                                 int(times)))
 
 if __name__ == "__main__":
     app.run()
